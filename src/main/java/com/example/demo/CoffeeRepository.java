1186c3ce62d98f41b3d553fb18870083c5499622
helloworld <ura22051995@gmail.com>
2023-04-20 12:36:48 +0300

    Создание приложения Coffee Spring 1

---
+++ src/main/java/com/example/demo/CoffeeRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+interface CoffeeRepository extends CrudRepository<Coffee, String> {
+}
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "droid")
+    Droid createDroid() {
+        return new Droid();
+    }
 
 }
+
+
